a6c6c46610d7d4f0b1192e68799889e10d705e25
주동호 <100278301+JOODON@users.noreply.github.com>
2022-09-06 00:16:36 +0900

    Initial commit

---
+++ src/main/java/SQL/SQLMaster.java
@@ -0,0 +1,14 @@
+package SQL;
+
+public class SQLMaster {
+    public static void main(String[] args) {
+        String SeleteAll="SELETE * FROM dataBase";
+
+        String Post = "--";
+
+        String SeleteCol="Selete CustomerNameCol FROM dataBase";
+        //중복 선택이 가능합니다!
+
+        String SeleteRow="Selete CustomerNameRow FROM dataBase";
+    }
+}
